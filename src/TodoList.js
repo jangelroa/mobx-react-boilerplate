a96cd5ab678f7c0ca69e6f799b89d085ce3c1eec
jangelroa <jangelroa@gmail.com>
2017-05-30 04:04:18 -0700

    Finished video2

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,7 +3,52 @@ import { observer } from "mobx-react";
 
 @observer
 export default class TodoList extends React.Component {
+  createNew(e) {
+    if (e.which === 13) {
+      this.props.store.createTodo(e.target.value);
+      e.target.value = "";
+    }
+  }
+
+  filter(e) {
+    this.props.store.filter = e.target.value;
+  }
+
+  toggleComplete(todo) {
+    todo.complete = !todo.complete;
+  }
+
   render() {
-    return <h1>{this.props.store.todos[0]}</h1>;
+    const {
+      showAll,
+      clearComplete,
+      filter,
+      filteredTodos,
+      todos
+    } = this.props.store;
+    const todoList = filteredTodos.map(todo => (
+      <li key={todo.id}>
+        <input
+          type="checkbox"
+          onChange={this.toggleComplete.bind(this, todo)}
+          value={todo.complete}
+          checked={todo.complete}
+        />
+        {todo.value}
+      </li>
+    ));
+    return (
+      <div>
+        <h1>todos</h1>
+        <input className="create" onKeyPress={this.createNew.bind(this)} />
+        <input
+          className="filter"
+          value={filter}
+          onChange={this.filter.bind(this)}
+        />
+        <ul>{todoList}</ul>
+        <a href="#" onClick={clearComplete}>Clear Complete</a>
+      </div>
+    );
   }
 }
