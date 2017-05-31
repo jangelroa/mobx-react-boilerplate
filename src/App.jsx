3d9d5743fe2c0fc166e789ceb013747f6b9326f7
jangelroa <jangelroa@gmail.com>
2017-05-30 23:14:13 -0700

    Moved appState out of the Counter class as a separate store

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,47 @@ import React, { Component } from "react";
 import { observer } from "mobx-react";
 import DevTools from "mobx-react-devtools";
 import { observable } from "mobx";
-import Counter from "./Counter";
+
+const appState = observable({
+  count: 0
+});
+
+appState.increment = function() {
+  this.count++;
+};
+
+appState.decrement = function() {
+  this.count--;
+};
+
+@observer class Counter extends Component {
+  render() {
+    return (
+      <div>
+        Counter: {this.props.store.count} <br />
+        <button onClick={this.handleInc}>+</button>
+        <button onClick={this.handleDec}>-</button>
+      </div>
+    );
+  }
+
+  handleInc = () => {
+    this.props.store.increment();
+  };
+  handleDec = () => {
+    this.props.store.decrement();
+  };
+
+  onReset = () => {
+    this.props.appState.resetTimer();
+  };
+}
 
 @observer class App extends Component {
   render() {
     return (
       <div>
-        <Counter />
+        <Counter store={appState} />
       </div>
     );
   }
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import { AppContainer } from "react-hot-loader";
-import AppState from "./AppState";
 import App from "./App";
-
-const appState = new AppState();
 
 render(
   <AppContainer>
